70e6a93b8086b726a9288865c1132fc6420eccb6
Blaine Miller <blaine141@gmail.com>
2020-09-21 14:15:23 -0400

    Reducing resource footprint

--- scripts.js
+++ scripts.js
@@ -5,7 +5,6 @@ function openProject(project) {
     $("#projectFrame").attr("src", "projects/"+project+".html")
     document.getElementById("mySideproject").style.width = ($("#viewport").width()) + "px";
     document.getElementById("viewport").style.marginRight = ($("#viewport").width() * .95) + "px";
-    $("li").toggleClass("nav-collapsed");
     projectOpen = true;
 }
 
@@ -13,12 +12,9 @@ function openProject(project) {
 function closeProject() {
     document.getElementById("mySideproject").style.width = "0";
     document.getElementById("main").style.marginRight = "0";
-    $("li").toggleClass("nav-collapsed");
     projectOpen = false;
 }
-$("#viewport").scroll(function () {
-    $(".title-div").css("opacity", 1 - $("#viewport").scrollTop() / $(document).height() * 4);
-});
+
 $(document).on('click', 'a', function (event) {
     if ($(this).attr('href')) {
         if ($(this).attr('href') == "#about") {
@@ -28,15 +24,16 @@ $(document).on('click', 'a', function (event) {
         if ($(this).attr('href')[0] == '#') {
             event.preventDefault();
             navigating = true;
-            console.log($($.attr(this, 'href')).offset().top)
             $('html, #viewport').animate({
                 scrollTop: $($.attr(this, 'href')).offset().top + $("#viewport").scrollTop()
             }, 500, function() {navigating = false});
         }
     }
 });
+
 var lnStickyNavigation = $("#viewport").height() - 60;
 updateHeight();
+
 $(window).on('resize', function () {
     updateHeight();
     if (projectOpen) {
@@ -48,14 +45,18 @@ $(window).on('resize', function () {
         $("#main").toggleClass("transitions");
     }
 });
+
 function updateHeight() {
     lnStickyNavigation = $(window).height() - 60;
 
     $('.navbar').css({ top: ($(window).height()) + 'px' });
     $('.jumbotron').css({ height: ($(window).height()) + 'px' });
 }
+
 $("#viewport").on('scroll', function () {
     stickyNavigation();
+    if ($("#viewport").scrollTop() < $(document).height())
+        $(".title-div").css("opacity", 1 - $("#viewport").scrollTop() / $(document).height() * 4);
     if(!navigating) {
         $(".navbar .nav a").each(function (index, element) {
             if(element.hash[0] == '#'){
@@ -67,12 +68,12 @@ $("#viewport").on('scroll', function () {
         })
     }
 });
+
 $(".navbar .nav > li").on('click', function () {
-    if (!$(this).hasClass("nav-back")) {
-        $(".navbar .nav > li").removeClass('active');
-        $(this).addClass('active');
-    }
+    $(".navbar .nav > li").removeClass('active');
+    $(this).addClass('active');
 });
+
 stickyNavigation();
 function stickyNavigation() {
     if ($("#viewport").scrollTop() > lnStickyNavigation) {
